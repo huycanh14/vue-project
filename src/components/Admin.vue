262fa40256a883514c18ec89ddcf9b6e1337aca3
Huy Canh <huycanh14@gmail.com>
2020-03-22 18:16:57 +0700

    Listing current orders

---
+++ src/components/Admin.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <h3>Menu:</h3>
+                <table class="table table-hover">
+                    <thead class="thead-defult">
+                        <tr>
+                            <th>Item</th>
+                            <th>Remove from menu</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Margheita</td>
+                            <td><button class="btn btn-outline-danger btn-sm">X</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>Current orders:</h3>
+                <table class="table table-hover">
+                    <thead class="thead-defult">
+                        <tr>
+                            <th>Item</th>
+                            <th>Size</th>
+                            <td>Quantity</td>
+                            <td>Price</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <div class="order-number">
+                            <strong><em>Order number: 1</em></strong>
+                            <button class="btn btn-outline-danger btn-sm">X</button>
+                        </div>
+                        <tr>
+                            <td>Margheita</td>
+                            <td>9"</td>
+                            <td>1</td>
+                            <td>6.95</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,17 +9,46 @@
                         <td>Add to basket</td>
                     </tr>
                 </thead>
-                <tbody v-for="(item, index) in getMenuItems">
+                <tbody v-for="(item, i) in getMenuItems" :key="`A-${i}`">
                     <tr>
                         <td><strong>{{ item.name }} </strong></td>
                     </tr>
-                    <tr v-for="option in item.options">
+                    <tr v-for="(option, index) in item.options" :key="index">
                         <td>{{ option.size }}</td>
                         <td> {{ option.price }} </td>
-                        <td><button class="btn btn-sm btn-outline-success" type="button">+</button></td>
+                        <td><button class="btn btn-sm btn-outline-success" type="button" @click="addToBask(item, option)">+</button></td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <div v-if="basket.length > 0">
+                <table class="table table-hover">
+                    <thead class="thead-default">
+                        <tr>
+                            <td>Quantity</td>
+                            <td>Item</td>
+                            <td>Total</td>
+                        </tr>
+                    </thead>
+                    <tbody v-for="(item, index) in basket" :key="`item-${index}`">
+                        <tr>
+                            <td>
+                                <button class="tn-secondary btn-sm" type="button" @click="decreaseQuantity(item)">-</button>
+                                <span> {{ item.quantity }} </span>
+                                <button class="tn-secondary btn-sm" type="button" @click="increaseQuantity(item)">+</button>
+                            </td>
+                            <td>{{ item.name }}  {{ item.size }}</td>
+                            <td>{{ item.price * item.quantity }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Order total: </p>
+                <button class="btn btn-success btn-block">Place Order</button>
+            </div>
+            <div v-else>
+                <p>{{ basketText }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -28,6 +57,7 @@
     export default {
         data() {
             return {
+                basket:[],
                 getMenuItems: {
                     1: {
                         'name': 'Margherita',
@@ -63,8 +93,30 @@
                         }]
                     }
 
-                }
+                },
+                basketText:"Your basket is empty",
             }
+        },
+        methods: {
+            addToBask(item, option) {
+                this.basket.push({
+                    name: item.name,
+                    price: option.price,
+                    size: option.size,
+                    quantity: 1
+                })
+            },
+            removeFromBasket(item){
+                this.basket.splice(this.basket.indexOf(item), 1);
+            },
+            increaseQuantity(item){
+                item.quantity++;
+            },
+            decreaseQuantity(item) {
+                item.quantity--;
+                if(item.quantity == 0)
+                    this.removeFromBasket(item);
+            },
         },
     }
 </script>
