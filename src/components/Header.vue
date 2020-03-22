3108fb7fb7abaeabb20e38d6d34e5f89d5a75d59
Huy Canh <huycanh14@gmail.com>
2020-03-22 12:42:54 +0700

    Styling our components

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="row">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="/">PIZZA PLANET</a>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a href="/" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="/" class="nav-link">Menu</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style>
+  header {
+    margin-bottom: 20px;
+  }
+
+  .navbar-brand {
+    font-size: 1.5em;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <div id="home" class="col-sm-12 text-center">
+            <div class="background">
+                <h1>Welcome to Pizza Planet!</h1>
+                <h2>Feeling hangry?</h2>
+                <button class="btn btn-success">Let's order!</button>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+#home{
+    background: url('../assets/pizza.jpg');
+    height: 800px;
+    padding: 10%;
+}
+h1, h2{
+    margin: 6%;
+}
+.background{
+    background: #eee;
+    opacity: 0.8;
+    max-width: 60%;
+    margin: 0 auto;
+    padding: 20px 0;
+}
+</style>
