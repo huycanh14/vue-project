68af31ac014c423d9519d03b5b5fe92e5564395e
Huy Canh <huycanh14@gmail.com>
2020-03-24 22:11:13 +0700

    Final components

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card" >
+    <div class="card-body">
+      <h5 class="card-title">Contact</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+      <p class="card-text">
+        <address>
+            <strong>Pizza Planet</strong> <br>
+            Address line 1 <br>
+            Address line 2 <br>
+            Phone: (+84) 983.746.278
+        </address>
+        <address>
+            <strong>Email: canh.huy.web@gmail.com</strong> <br>
+            <p>contact@pizzaplanet.com</p>
+        </address>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
